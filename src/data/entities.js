2e9b0e3c8b002b279692ff3029b16937bc8f0e99
Alina Pokhomova <pokhomova.alina@mail.ru>
2026-07-10 11:02:38 +0300

    Создана структура проекта и добавлен список сущностей

---
+++ src/data/entities.js
@@ -0,0 +1,19 @@
+export const entities = [
+    {
+        id: "leads",
+        title: "Сделки",
+        description: "Методы для работы со сделками CRM."
+    },
+
+    {
+        id: "contacts",
+        title: "Контакты",
+        description: "Методы для работы с контактами CRM."
+    },
+
+    {
+        id: "companies",
+        title: "Компании",
+        description: "Методы для работы с компаниями CRM."
+    }
+];
