55e1a63e556270effa4c9b831798f0c077e84a97
Alina Pokhomova <pokhomova.alina@mail.ru>
2026-07-10 11:07:51 +0300

    Создан каркас главной страницы

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Информационная система поиска и анализа API-запросов amoCRM</h1>
+      <p>Главная страница</p>
+    </div>
+  );
+}
+
+export default Home;
